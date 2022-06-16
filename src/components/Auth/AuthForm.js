91a41af0cf896f52aec85b4d011d71c54458222b
Bokayy X <blabsy8@gmail.com>
2022-06-16 22:12:57 +0200

    fixed validation

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,18 +1,58 @@
 import { useState, useRef, useContext } from "react";
-import{ useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
+import useInput from "../../hooks/userInput";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const history = useHistory();
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
+  // const emailInputRef = useRef();
+  // const passwordInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+  const validEmailFormat = /^\S+@\S+\.\S{2}/;
+
+  //email
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailInputHasError,
+    inputChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    resetInputs: resetEmailInput,
+  } = useInput((value) => value.match(validEmailFormat));
+
+  //password
+  let specialChars = /[!#$+-]/;
+  const hasNumber = /\d/;
+  const {
+    value: enteredPassword,
+    isValid: passwordIsValid,
+    hasError: passwordInputHasError,
+    inputChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    resetInputs: resetPasswordInput,
+  } = useInput((value) =>{
+    if(value.trim().length > 5){
+      if(hasNumber.test(value)){
+        if(specialChars.test(value)){
+          return true;
+        }
+      }
+    }
+  });
+    
+    /* console.log('trimmed value', value.trim().length) &&
+      (value.trim().length > 5) &&
+      console.log('hasNumber test: ', hasNumber.test(value))&&
+      hasNumber.test(value) &&
+      console.log('has specialChars test: ', specialChars.test(value))&&
+      specialChars.test(value)); */
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -21,8 +61,12 @@ const AuthForm = () => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const enteredEmail = emailInputRef.current.value;
-    const enteredPassword = passwordInputRef.current.value;
+    if (!emailIsValid || !passwordIsValid) {
+      return;
+    }
+
+    // const enteredEmail = emailInputRef.current.value;
+    // const enteredPassword = passwordInputRef.current.value;
 
     //dodat auth
 
@@ -52,23 +96,37 @@ const AuthForm = () => {
           return res.json();
         } else {
           return res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
+            // let errorMessage = "Authentication failed!";
             // if (data && data.error && data.error.message) {   //proslijeduje firebase error
             //   errorMessage = data.error.message;
             // }
+            let errorMessage = "";
+            if (data.error.message === "INVALID_PASSWORD") {
+              errorMessage = "Invalid password";
+            } else if (data.error.message === "EMAIL_EXISTS") {
+              errorMessage = "Email already exists";
+            } else if (data.error.message === "INVALID_EMAIL") {
+              errorMessage = "Email is invalid!";
+            } else {
+              errorMessage = "This e-mail does not exist";
+            }
 
             throw new Error(errorMessage);
           });
         }
       })
       .then((data) => {
-        const expirationTime = new Date(new Date().getTime() + (+data.expiresIn *1000));
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
         authCtx.login(data.idToken, expirationTime.toISOString());
-        history.replace('/adresar');
+        history.replace("/adresar");
       })
       .catch((err) => {
         alert(err.message);
       });
+    resetEmailInput();
+    resetPasswordInput();
   };
 
   return (
@@ -77,7 +135,20 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required ref={emailInputRef} />
+          <input
+            type="email"
+            id="email"
+            required
+            //ref={emailInputRef}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+            value={enteredEmail}
+          />
+          {emailInputHasError && (
+            <p className={classes["error-msg"]}>
+              Please enter a valid email address
+            </p>
+          )}
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
@@ -85,8 +156,17 @@ const AuthForm = () => {
             type="password"
             id="password"
             required
-            ref={passwordInputRef}
+            // ref={passwordInputRef}
+            onChange={passwordChangeHandler}
+            onBlur={passwordBlurHandler}
+            value={enteredPassword}
           />
+          {passwordInputHasError && (
+            <p>
+              Password must be at least 6 characters long and contain at least
+              one number and one special character
+            </p>
+          )}
         </div>
         <div className={classes.actions}>
           {!isLoading && (
